361d202d1c1d0c567f79eff9698a94cf89d74026
SamyBenn <2031008@cegeprdl.ca>
2022-02-15 10:15:09 -0500

    Ajouter Enseignant

--- Controllers/EnseignantController.cs
+++ Controllers/EnseignantController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using TpWeb.Logics.Controleurs;
+using TpWeb.Models;
 
 namespace TpWeb.Controllers
 {
@@ -18,14 +19,14 @@ namespace TpWeb.Controllers
                 {
                     cegep = CegepControleur.Instance.ObtenirListeCegep()[0].Nom;
                 }
-                ViewBag.Cegep = CegepControleur.Instance.ObtenirCegep(cegep);
+                ViewBag.nomCegep = cegep;
                 ViewBag.ListeDepartements = CegepControleur.Instance.ObtenirListeDepartement(cegep).ToArray();
-                ViewBag.cegep = cegep;
+
                 if (departement is null)
                 {
                     departement = CegepControleur.Instance.ObtenirListeDepartement(cegep)[0].Nom;
                 }
-                ViewBag.Departement = CegepControleur.Instance.ObtenirDepartement(cegep, departement);
+                ViewBag.nomDepartement = CegepControleur.Instance.ObtenirDepartement(cegep, departement).Nom;
                 ViewBag.ListeEnseignant = CegepControleur.Instance.ObtenirListeEnseignant(cegep, departement).ToArray();
             }
             catch (Exception e)
@@ -33,6 +34,21 @@ namespace TpWeb.Controllers
                 ViewBag.MessageErreur = e.Message;
             }
             return View();
+        }
+
+        [Route("Enseignant/AjouterEnseignant")]
+        [HttpPost]
+        public ActionResult AjouterEnseignant([FromForm]string nomCegep, [FromForm]string nomDepartement, [FromForm]EnseignantDTO enseignant)
+        {
+            try
+            {
+                CegepControleur.Instance.AjouterEnseignant(nomCegep, nomDepartement, enseignant);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index", "Enseignant", new { cegep = nomCegep, departement = nomDepartement });
         }
     }
 }
